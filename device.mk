29e1ea80ccb280e52dd79b87daaa513bfa64d295
Soyab Khan <56043965+soyabkhanmalek@users.noreply.github.com>
2024-09-29 23:51:47 +0530

    add BCR

--- device.mk
+++ device.mk
@@ -10,6 +10,8 @@ $(call inherit-product, vendor/xiaomi/sunny/sunny-vendor.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+#BCR
+$(call inherit-product, vendor/bcr/bcr.mk)
 # A/B
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
